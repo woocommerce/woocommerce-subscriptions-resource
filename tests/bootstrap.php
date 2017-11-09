5452a70405dfaa5e5288b85fd420d0cfbc2d9c75
matttallan <matt.d.a@live.com>
2017-11-09 12:32:05 +1000

    Add class_alias to fix tests for PHP 7.0+
    - I decided to copy over the full list of class aliases used in WP core incase we need them.
      But for the time being, just loading the TestCase class is all we need

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -38,6 +38,21 @@ class WCSR_Unit_Tests_Bootstrap {
 		require_once( 'mocks/class-wc-data.php' );
 
 		require_once( $this->plugin_dir . '/includes/class-wcsr-resource.php' );
+
+		// Load relevant class aliases for PHPUnit 6 (ran on PHP v7.0+ in Travis)
+		if ( class_exists( 'PHPUnit\Runner\Version' ) && version_compare( PHPUnit\Runner\Version::id(), '6.0', '>=' ) ) {
+			class_alias( 'PHPUnit\Framework\TestCase', 'PHPUnit_Framework_TestCase' );
+			// class_alias( 'PHPUnit\Framework\Exception', 'PHPUnit_Framework_Exception' );
+			// class_alias( 'PHPUnit\Framework\ExpectationFailedException', 'PHPUnit_Framework_ExpectationFailedException' );
+			// class_alias( 'PHPUnit\Framework\Error\Notice', 'PHPUnit_Framework_Error_Notice' );
+			// class_alias( 'PHPUnit\Framework\Test', 'PHPUnit_Framework_Test' );
+			// class_alias( 'PHPUnit\Framework\Warning', 'PHPUnit_Framework_Warning' );
+			// class_alias( 'PHPUnit\Framework\AssertionFailedError', 'PHPUnit_Framework_AssertionFailedError' );
+			// class_alias( 'PHPUnit\Framework\TestSuite', 'PHPUnit_Framework_TestSuite' );
+			// class_alias( 'PHPUnit\Framework\TestListener', 'PHPUnit_Framework_TestListener' );
+			// class_alias( 'PHPUnit\Util\GlobalState', 'PHPUnit_Util_GlobalState' );
+			// class_alias( 'PHPUnit\Util\Getopt', 'PHPUnit_Util_Getopt' );
+		}
 	}
 
 	/**
