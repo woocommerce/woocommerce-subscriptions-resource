e262d4a1fd1f3addbc318c5b3a862c4029f2007b
matttallan <matt.d.a@live.com>
2017-11-23 09:56:06 +1000

    Don't break existing functions hooked onto the line item name filter
     - AXIOM was hooked onto this filter and expected the 2nd param to be
       the resource, but this was moved around when moving the code.
     - this commit brings the order back to the apply_filters

--- includes/wcsr-renewal-functions.php
+++ includes/wcsr-renewal-functions.php
@@ -27,7 +27,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 function wcsr_get_line_item_name( $line_item, $days_active, $days_in_period, $resource = null, $from_timestamp = 0, $to_timestamp = 0 )  {
 	$line_item_name = ( $days_active != $days_in_period ) ? sprintf( '%s usage for %d of %d days.', $line_item->get_name(), $days_active, $days_in_period ) : $line_item->get_name();
 
-	return apply_filters( 'wcsr_renewal_line_item_name', $line_item_name, $line_item, $days_active, $days_in_period, $resource, $from_timestamp, $to_timestamp );
+	return apply_filters( 'wcsr_renewal_line_item_name', $line_item_name, $resource, $line_item, $days_active, $days_in_period, $from_timestamp, $to_timestamp );
 }
 
 /**
